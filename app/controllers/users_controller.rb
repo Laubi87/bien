40f2bd4bb7cc9a7c3614f38441ec905c501e351c
Simon Laubscher <simon.laubscher@web.de>
2020-05-23 18:37:39 +0200

    added user form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    #form for adding  a new user
+    @user = User.new
+  end
+  def create
+    #take the params from the form
+    #create a new user
+    @user = User.new(form_params)
+    #if its valid and it save go to the list users pages
+    #if not see the form with error
+    if @user.save
+      redirect_to users_path
+    else
+      render "new"
+    end
+  end
+
+  def form_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
