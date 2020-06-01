6d8446e0287258f3c026226b22d588e379e01591
Simon Laubscher <simon.laubscher@web.de>
2020-06-01 12:49:23 +0200

    fixed bug

--- db/migrate/20200523083430_add_location_to_reviews.rb
+++ db/migrate/20200523083430_add_location_to_reviews.rb
@@ -1,6 +1,6 @@
 class AddLocationToReviews < ActiveRecord::Migration[6.0]
   def change
-    add_column :reviews, :address, :text_field
+    add_column :reviews, :address, :text
     add_column :reviews, :latitude, :float
     add_column :reviews, :longitude, :float
   end
