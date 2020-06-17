cd411017dd3122f745cec0a3200b2b0c5b6dc45b
Simon Laubscher <simon.laubscher@web.de>
2020-06-17 12:48:57 +0200

    fixed leak of AWS keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAIWRC2MI6S2TU5SVA',                        # required unless using use_iam_profile
-    aws_secret_access_key: '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',                        # required unless using use_iam_profile
+    aws_access_key_id:     'xxx',                        # required unless using use_iam_profile
+    aws_secret_access_key: 'yyy',                        # required unless using use_iam_profile
   }
   config.fog_directory  = 'bien-review'                                      # required
 end
